5b50df76fe0ea73c40e4c42564caa1887f31ebb3
Armani Johnson <armanijo@usc.edu>
2025-04-07 03:33:39 +0000

    first commit

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,12 +8,28 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
+bool checkPaths(Node* node, int currentDep, int& leafDep);
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
-    if(root->left )
+    int depth = -1;
+    return checkPaths(root, 0, depth);
+}
 
+
+bool checkPaths(Node* node, int currDep, int& leafDep){
+  if(node == nullptr){
+    return true;
+  }
+
+  if(node->left == nullptr && node->right == nullptr) {
+    if(leafDep == -1){
+      leafDep = currDep;
+      return true;
+    } else{
+      return currDep == leafDep;
+    }
+  }
+  return checkPaths(node->left, currDep + 1, leafDep) && checkPaths(node->right, currDep + 1, leafDep);
 }
 
